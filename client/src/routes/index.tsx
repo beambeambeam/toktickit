85531dd6d1b772c2efec5dc0c265bd79eef9f8b3
beambeambeam <supawitmarayat@gmail.com>
2026-08-04 14:45:41 +0700

    feat; add oxlint and oxfmt

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+const Index = () => (
+  <main className="page">
+    <h1>Welcome Home!</h1>
+  </main>
+);
+
+export const Route = createFileRoute("/")({
   component: Index,
-})
-
-function Index() {
-  return (
-    <main className="page">
-      <h1>Welcome Home!</h1>
-    </main>
-  )
-}
+});
