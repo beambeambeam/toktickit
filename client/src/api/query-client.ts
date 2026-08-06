27558aa7d462d57dca2654e37a3ad7c7bb116699
beambeambeam <supawitmarayat@gmail.com>
2026-08-06 22:22:20 +0700

    chore: configure query defaults

--- client/src/api/query-client.ts
+++ client/src/api/query-client.ts
@@ -1,3 +1,16 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      retry: false,
+    },
+    queries: {
+      gcTime: 600_000,
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: true,
+      retry: 2,
+      staleTime: 60_000,
+    },
+  },
+});
