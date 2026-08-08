10fabfa0fb34e4dfd0a310a23bc64e3c1f55c2f5
beambeambeam <supawitmarayat@gmail.com>
2026-08-08 11:37:49 +0700

    feat: add category schema migration (#21)

---
+++ server/prisma/migrations/20260808113350_add_category/migration.sql
@@ -0,0 +1,14 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
